a77ee95676061931678d78cb2d18bd7b425a49ee
ankielbowicz <an.kielbowicz@gmail.com>
2020-04-04 14:42:33 +0200

    repair code to flake8

--- model/sales/sales.py
+++ model/sales/sales.py
@@ -1,13 +1,13 @@
 """ Sales module """
 
-# modules import go here: 
+# modules import go here:
 import random
 
 from model import data_manager
 from model.store import store
 from model.hr import hr
 
-# global variables go here: 
+# global variables go here:
 
 CUSTOMER_ID = 2
 PRODUCT_ID = 3
@@ -18,10 +18,12 @@ STORE_MANUFACTURER = 2
 
 # sales.csv: 'id, employee_id, customer_id, product_id, number_of_items'
 
+
 def get_table():
     file = "model/sales/sales.csv"
     table = data_manager.get_table_from_file(file)
     return table
+
 
 def generate_random(table):
     """
@@ -45,10 +47,10 @@ def generate_random(table):
     while generated in id_list or len(generated) != expected_length:
         quantity = 2
         alphabet = 'abcdefghijklmnopqrstuvwxyz'
-        characters = '!@#$%^&*()_+=-{}[]|\:"<>,.?/'
-        random_letters = [random.choice(alphabet) for i in range (quantity)] + [random.choice(alphabet).upper() for i in range (quantity)]
-        random_characters = [random.choice(characters) for i in range (quantity)]
-        random_numbers = [str(random.randint(0, 9)) for i in range (quantity)]
+        characters = '!@#$%^&*()_+=-{}[]|\\:"<>,.?/'
+        random_letters = [random.choice(alphabet) for i in range(quantity)] + [random.choice(alphabet).upper() for i in range (quantity)]
+        random_characters = [random.choice(characters) for i in range(quantity)]
+        random_numbers = [str(random.randint(0, 9)) for i in range(quantity)]
         generated_list = [letter for letter in random_letters] + [character for character in random_characters] + [digit for digit in random_numbers]
         random.shuffle(generated_list)
         generated = "".join(generated_list)
@@ -59,13 +61,13 @@ def generate_random(table):
 def add_transaction(table, id, store_id, hr_id, crm_id, quantity):
     """ take id, then pair it with corresponding hr_id, customer_id, product_id, and add numbers of bought items,
     put it into new record
-    
-    Args: 
+
+    Args:
     list (of lists) - table from sales
-    int - id 
+    int - id
     int - id from store
     int - id from hr"
-    int - id from crm 
+    int - id from crm
     int - quantity of bought items
     """
     record = [id, store_id, hr_id, crm_id, quantity]
@@ -87,14 +89,14 @@ def filter_by_employee(table, employee_id):
 def filter_by_customer(table, customer):
     """
     Filter transactions in search of deals with a specific customer.
-    
-    Args: 
-        table (list): 
+
+    Args:
+        table (list):
         customer (): customer's id
 
     Returns:
-        transactions (list):        
-    
+        transactions (list):
+
     """
     transactions = []
     for record in table:
@@ -106,7 +108,7 @@ def filter_by_customer(table, customer):
 def filter_by_manufacturer(table, manufacturer):
     """
     Filter transactions to get sales of games per a specific manufacturer.
-    
+
     Args:
         table (list):
         manufacturer (string): manufacturer's name
@@ -119,7 +121,7 @@ def filter_by_manufacturer(table, manufacturer):
     for record in store_table:
         if record[STORE_MANUFACTURER] == manufacturer:
             store_id_list.append(record[STORE_PRODUCT_ID])
-    
+
     sales = 0
     for record in table:
         if record[PRODUCT_ID] in store_id_list:
@@ -205,30 +207,31 @@ def rank_by_manufacturer(table):
 
 
 def get_sold_copies(table, store_id):
-    """ Gets total number of sold copies by id, 
-    
+    """ Gets total number of sold copies by id,
+
     Args:
     list (of lists) - sales database
     string - item id in store database """
-    
+
     store_id_position = 1
     quantity_position = 4
     sold_copies = 0
 
-    for record in table: 
+    for record in table:
         if str(record[store_id_position]) == str(store_id):
             sold_copies = sold_copies + int(record[quantity_position])
-        
+
     return sold_copies
 
+
 def generate_raport(table):
-    """ Generate a report of sold items and earned money per game. 
-    
-    Args: 
+    """ Generate a report of sold items and earned money per game.
+
+    Args:
     list(of lists) - list from store containing items
     list(of lists) - list from sales containing, among others, number of sold copies
-    
-    Return: 
+
+    Return:
     list(of lists) - containing items and money earned per item """
 
     item_table = store.get_table()
@@ -238,9 +241,9 @@ def generate_raport(table):
     id_position = 0
     title_position = 1
     price_position = 3
-    earnings = 0 
+    earnings = 0
 
-    for item in item_table: 
+    for item in item_table:
         price = item[price_position]
         sold_copies = get_sold_copies(table, item[id_position])
         if price and sold_copies:
@@ -248,5 +251,5 @@ def generate_raport(table):
             print(f"price = {price}, sold copies = {sold_copies}, earnings = {earnings}")
             raport_row = [item[title_position], str("earnings")]
             raport.append(raport_row)
-    
+
     return raport
